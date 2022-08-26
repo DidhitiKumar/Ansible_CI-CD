d8efa126964b3abd7a450c6d7aa96bef6b0ca50d
DidhitiKumar <62550336+DidhitiKumar@users.noreply.github.com>
2022-08-26 16:22:06 +0530

    Update Python_script_run.py

--- Python_script_run.py
+++ Python_script_run.py
@@ -1,3 +1,10 @@
 # This program prints Hello, world!
-
-print('Hello, world!')
+--- 
+- name: "Python Script run"
+  become: true
+  gather_facts: false
+  hosts: all
+  strategy: free
+  tasks: 
+    - name: Run the HelloWorld File
+      command: python3 HelloWorld.py
